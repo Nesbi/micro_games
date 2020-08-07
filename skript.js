391bea4181a2a71f0f1b9c20ce4203c3c0be1689
Nico Balbach <Nico.Balbach@gmail.com>
2020-08-07 23:12:56 +0200

    Player controller

--- skript.js
+++ skript.js
@@ -16,44 +16,57 @@ let blocks = [
 	[10,40],[30,40],[50,40],[70,40],[90,40],[110,40],[130,40],[150,40],[170,40],
 ];
 
+//Control
+let pDX = 0;
+let rDown = false;
+let lDown = false;
+document.addEventListener("keydown", event => {
+	if(event.keyCode == 39) rDown = true; // ArrowRight
+	if(event.keyCode == 37) lDown = true; // ArrowLeft
+})
+
+document.addEventListener("keyup", event => {
+	if(event.keyCode == 39) rDown = false; // ArrowRight
+	if(event.keyCode == 37) lDown = false; // ArrowLeft
+	console.log("up", event.keyCode);
+})
+//Physics
 let dX = 0;
-let dY = 5;
+let dY = 1;
 
 const collide = (xBox, yBox, boxWidth, boxHeight) => {
 	// approx ball as rectangle
 	if(xBox>=ball[0]+ballRadius || ball[0]-ballRadius>=xBox+boxWidth) return false;
 	if(yBox>=ball[1]+ballRadius || ball[1]-ballRadius>=yBox+boxHeight) return false;
-	return true;
-}
 
-physics = () => {
+	// Collision
+	const x = ball[0]-(xBox+boxWidth/2);
+	const y = ball[1]-(yBox+boxHeight/2);
+	dX = x/Math.abs(x+y);
+	dY = y/Math.abs(x+y);
+}
+setInterval(() => {
 	ball[0]+=dX;
 	ball[1]+=dY;
 	//bounds
 	if(ball[0]-ballRadius<=0||ball[0]+ballRadius==width)dX*=-1;//left | right
 	if(ball[1]-ballRadius<=0||ball[1]+ballRadius==height)dY*=-1;//top | bottom
 	//player
-	if(collide(player[0],player[1],playerWidth,playerHeight)) {
-		dX*=-1;
-		dY*=-1;
-	}
+	collide(player[0],player[1],playerWidth,playerHeight);
 	
 	//blocks
-	blocks.forEach(b => {
-		if(collide(b[0],b[1],blockWidth,blockHeight)) {
-			dX*=-1;
-			dY*=-1;
-		}
-	})
+	blocks.forEach(b => collide(b[0],b[1],blockWidth,blockHeight));
 
-}
+	// Player controlls
+	player[0] += rDown - lDown;
+}, 50);
 
+// Draw
 c = document.body.children[0];
 c.height=height;
 c.width=width;
 ctx = c.getContext("2d");
-
-draw = () => {
+setInterval(() => {
 	//clear
 	ctx.clearRect(0, 0, c.width, c.height);
 	//player
@@ -64,6 +77,4 @@ draw = () => {
 	ctx.fill();
 	//blocks
 	blocks.forEach(e=>ctx.fillRect(e[0]+1,e[1]+1,blockWidth-2,blockHeight-2));
-}
-setInterval(() => {physics()}, 100);
-setInterval(() => {draw()}, 100);
+}, 100);
