34b52566f69c4893e3b95434171337f8befd80ea
Nico Balbach <Nico.Balbach@gmail.com>
2020-08-07 22:41:01 +0200

    Collision

--- skript.js
+++ skript.js
@@ -8,6 +8,7 @@ let playerHeight = 10;
 let playerWidth = 80;
 let player = [100-playerWidth/2,160];
 let blockWidth = 20;
+let blockHeight = 10;
 let blocks = [
 	[10,10],[30,10],[50,10],[70,10],[90,10],[110,10],[130,10],[150,10],[170,10],
 	[10,20],[30,20],[50,20],[70,20],[90,20],[110,20],[130,20],[150,20],[170,20],
@@ -18,23 +19,11 @@ let blocks = [
 let dX = 0;
 let dY = 5;
 
-const boxcircle = (xBox, yBox, boxWidth, boxHeight) => {
-    var hw = boxWidth / 2
-    var hh = boxHeight / 2
-    var distX = Math.abs(ball[0] - (xBox + boxWidth / 2))
-    var distY = Math.abs(ball[1] - (yBox + boxHeight / 2))
-
-    if (distX > hw + ballRadius || distY > hh + ballRadius) {
-        return false;
-    }
-
-    if (distX <= hw || distY <= hh) {
-        return true;
-    }
-
-    var x = distX - hw
-    var y = distY - hh
-    return x * x + y * y <= ballRadius * ballRadius
+const collide = (xBox, yBox, boxWidth, boxHeight) => {
+	// approx ball as rectangle
+	if(xBox>=ball[0]+ballRadius || ball[0]-ballRadius>=xBox+boxWidth) return false;
+	if(yBox>=ball[1]+ballRadius || ball[1]-ballRadius>=yBox+boxHeight) return false;
+	return true;
 }
 
 physics = () => {
@@ -44,13 +33,18 @@ physics = () => {
 	if(ball[0]-ballRadius<=0||ball[0]+ballRadius==width)dX*=-1;//left | right
 	if(ball[1]-ballRadius<=0||ball[1]+ballRadius==height)dY*=-1;//top | bottom
 	//player
-	if(boxcircle(player[0],player[1],playerWidth,playerHeight)) {
-		console.log("hit!")
+	if(collide(player[0],player[1],playerWidth,playerHeight)) {
 		dX*=-1;
 		dY*=-1;
 	}
 	
 	//blocks
+	blocks.forEach(b => {
+		if(collide(b[0],b[1],blockWidth,blockHeight)) {
+			dX*=-1;
+			dY*=-1;
+		}
+	})
 
 }
 
@@ -69,7 +63,7 @@ draw = () => {
 	ctx.arc(ball[0],ball[1],ballRadius,0,2*Math.PI);
 	ctx.fill();
 	//blocks
-	blocks.forEach(e=>ctx.fillRect(e[0]+1,e[1]+1,blockWidth-2,8));
+	blocks.forEach(e=>ctx.fillRect(e[0]+1,e[1]+1,blockWidth-2,blockHeight-2));
 }
 setInterval(() => {physics()}, 100);
 setInterval(() => {draw()}, 100);
